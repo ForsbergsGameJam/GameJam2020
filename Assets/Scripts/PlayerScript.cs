7a04ca87bc869e11599aacf3168e2700f44b9e20
Robin Stjerndorff <robin@stjerndorff.org>
2020-01-18 20:52:26 +0100

    update MainScene

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public GameObject closest_valid_trapzone = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +20,40 @@ public class PlayerScript : MonoBehaviour
         // Check closest TrapZone
         GameObject[] trapZones = GameObject.FindGameObjectsWithTag("TrapZone");
 
+
+        Vector3 player_camera_pos = GetComponent<CharacterController>().transform.position;
+        //Parera med kamerans position ungefär
+        player_camera_pos.z += 0.0f;
+
+
+        closest_valid_trapzone = null;
+
         foreach (GameObject trapZone in trapZones)
         {
-            if (GetComponent<CharacterController>().transform.position.z < trapZone.transform.position.z)
-            {
+            if (GetComponent<CharacterController>().transform.position.z > trapZone.transform.position.z)
+                continue;
 
+            Debug.Log("player z: "+ GetComponent<CharacterController>().transform.position.z+"   trap zone z: " +trapZone.transform.position.z);
+            float dist_to_trapzone = Vector3.Distance(trapZone.transform.position, GetComponent<CharacterController>().transform.position);
+
+            if (closest_valid_trapzone == null)
+            {
+                closest_valid_trapzone = trapZone;
+            }
+
+            float dist_to_closest_trapzone = Vector3.Distance(closest_valid_trapzone.transform.position, player_camera_pos);
+
+            if ( (dist_to_trapzone < dist_to_closest_trapzone )) {
+                closest_valid_trapzone = trapZone;
             }
         }
 
+        if (closest_valid_trapzone != null && Vector3.Distance(closest_valid_trapzone.transform.position, player_camera_pos) > 20.0f )
+            closest_valid_trapzone = null;
+
+        if (closest_valid_trapzone)
+            Debug.LogFormat("Closest TrapZone is " + closest_valid_trapzone.gameObject.name);
+        else
+            Debug.Log("No closest TrapZone");
     }
 }
