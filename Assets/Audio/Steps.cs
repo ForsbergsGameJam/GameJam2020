8c54b8f89e061238b542aebae10f53e7f1c522fa
Akash Menon <akashmenon@live.se>
2020-01-19 13:05:41 +0100

    Footsteps

---
+++ Assets/Audio/Steps.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steps : MonoBehaviour
+{
+    public AudioClip[] stepClips;
+    public AudioSource player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Awake()
+    {
+        player = GetComponent<AudioSource>();
+    }
+
+    public void step() {
+        player.clip = stepClips[Random.Range(0, stepClips.Length)];
+        player.PlayOneShot(player.clip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
